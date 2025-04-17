f6ac64268434b1cc700d9b28b541a80df3da97db
TinotendaMugabe <mcdonaldmugs@gmail.com>
2025-04-17 15:49:15 +0200

    vercel auth

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,9 @@ import { setupVite, serveStatic, log } from "./vite";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/images', express.static(path.join(process.cwd(), 'public/images')));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.use((req, res, next) => {
   const start = Date.now();
